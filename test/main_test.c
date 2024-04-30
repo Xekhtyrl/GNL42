076d03b329aa15f14170d376c1df26549328b995
Leopoldo Vodak <lvodak@mi-r7-p2.s19.be>
2024-04-30 19:22:47 +0200

    test_file

---
+++ test/main_test.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include "get_next_line.h"
+
+int main(int argc, char **argv)
+{
+  char *str;
+  int fd = open(argv[argc - 1], O_RDONLY);
+  int i = 0;
+  while (++i)
+    {
+      str = get_next_line(fd);
+      printf("i = %s", i, str);
+      if (!str)
+        return (0);
+      free(str);
+    }
+}
